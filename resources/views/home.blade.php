4bac290476ef1c6b438f9c548eb1da8415e401e6
jhona06 <cestinakareljhona6@gmail.com>
2024-08-23 22:34:45 +0800

    jhona/resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,18 +9,16 @@
 
     <!-- Custom Styles -->
     <style>
+        
         /* Navbar */
-        .navbar-container {
+        .navbar {
             position: fixed;
             top: 0;
             width: 100%;
-            z-index: 9999;
-        }
-
-        .navbar {
-            background-color: #f8f9fa;
             padding-top: 16px;
             padding-bottom: 16px;
+            z-index: 9999;
+            background-color: #f8f9fa;
         }
 
         .navbar-brand {
@@ -34,10 +32,9 @@
             text-transform: uppercase;
             font-size: 14px;
         }
-
         /* Section Padding */
         .content-container {
-            padding-top: 100px; /* Adjusted padding to account for fixed navbar */
+            padding-top: 100px; 
             padding-bottom: 60px;
         }
 
@@ -90,8 +87,8 @@
 </head>
 <body>
 
-    <!-- Navbar Container -->
-    <div class="navbar-container">
+     <!-- Navbar Container -->
+ <div class="navbar-container">
         <!-- Navbar -->
         <nav class="navbar navbar-expand-lg">
             <div class="container">
@@ -101,8 +98,8 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#home">Home</a>
+                    <li class="nav-item">
+                            <a class="nav-link" href="home">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="about">About</a>
