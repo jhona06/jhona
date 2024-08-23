fc656e97c5402d30a0af960b7e856df5ada13a55
jhona06 <cestinakareljhona6@gmail.com>
2024-08-23 21:33:50 +0800

    jhona/resources/views/content.blade.php

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -114,33 +114,12 @@
                         <a class="nav-link" href="about">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="content">Menu</a>
+                        <a class="nav-link" href="content">Content</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
-
-    <!-- About Section -->
-    <section id="about" class="py-5 bg-light">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8 text-center">
-                    <h2 class="display-4 mb-4">About Our Restaurant</h2>
-                    <p class="lead mb-4">
-                        Welcome to Mac and Gab! We are committed to serving delicious, fresh, and healthy food
-                        made from the finest ingredients. Our passion for food is reflected in every dish we create,
-                        ensuring a memorable dining experience for our customers.
-                    </p>
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-6">
-                    <img src="{{ asset('images/restaurant.jpg') }}" class="img-fluid rounded shadow" alt="Restaurant">
-                </div>
-            </div>
-        </div>
-    </section>
 
     <!-- Menu Section (Content Section) -->
     <section id="content" class="py-5">
