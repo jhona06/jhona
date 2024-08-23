4acee4246a6f3eca7ad9041e859d3639d0516f7f
jhona06 <cestinakareljhona6@gmail.com>
2024-08-23 20:26:35 +0800

    jhona/resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,26 +18,23 @@
         </style>
     <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
     <ul class="nav justify-content-end">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="home">Home</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="about">About</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="content">Content</a>
-  </li>
+        <li class="nav-item">
+            <a class="nav-link" href="home">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="about">About</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="content">Content</a>
+        </li>
   
-</ul>
+    </ul>
       </ul>
     </div>
   </div>
