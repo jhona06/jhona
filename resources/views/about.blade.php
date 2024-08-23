d9eae6524fda87cfa1db4c68b2a31abd7bf18314
jhona06 <cestinakareljhona6@gmail.com>
2024-08-23 21:14:19 +0800

    jhona/resources/views/about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -63,20 +63,44 @@
             </div>
         </div>
         </nav>
-        <section id="hero">
-            <div class="container">
-                <div class="row">
-                <img src="C:\Users\Karel\Desktop\Laravel\jhona\public\images\landscape food.jpg" class="img-fluid" alt="Responsive image">
-                    <div class="col-lg-6">
-                    <img src="landscape food.jpg" class="img-fluid" alt="Responsive image">
-                        <h1 class="display-5" data-aos="fade-up">JHONA CESTINA</h1>
-                        <p data-aos="fade-up" data-aos-delay="300">Join me in exploring our shared  passion for technology as we delve into the depths of the tech world. Get to know more about me and my journey in the ever evolving realm of IT</p>
-                        <a href="#aboutme" class="btn btn-outline-light" data-aos="fade-up" data-aos-delay="500"> Know me more</a>
-                    </div>
-                </div>
-            </div>
-
-      </section>
+        <div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
         <script src="/bootstrap-5.3.3-dist/js/bootstrap.js"></script>
 </body>
