7aea7144cb87ce519035ad25edf68dfe4f6c1f09
jhona06 <cestinakareljhona6@gmail.com>
2024-08-23 18:16:30 +0800

    jhona/routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/content', function () {
+    return view('content');
+});
+Route::get('/about', function () {
+    return view('about');
+});
