cf0069eb83c01b3d227eefe1ec98dda1aa8ffe01
jhona06 <cestinakareljhona6@gmail.com>
2024-09-01 16:57:08 +0800

    resources/views/contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -30,7 +30,7 @@
 <body>
     <section id="contact">
         <div class="container">
-            <h2 class="text-left" margin-bottom: 20px;>Contact Us</h2>
+            <h2 class="text-left" >Contact Us</h2>
             <div class="row">
                 <div class="col-md-6">
                     <form action="{{ route('user.submit') }}" method="POST">
