9d709f4c68294cf699c3af8f004a0ccdd8c52b62
jhona06 <cestinakareljhona6@gmail.com>
2024-09-01 18:33:34 +0800

    resources/views/user.blade.php

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,54 +5,110 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
     <style>
+         .navbar-container {
+            position: fixed;
+            top: 0;
+            width: 100%;
+            z-index: 9999;
+        }
 
-body {
-    font-family: 'Playfair Display', serif;
-    font-weight: 700;
-    background-color: #f5f5f5;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh; 
-}
+        .navbar {
+            background-color: #ECDFCC;
+        }
 
-.content {
-    flex: 1; 
-    display: flex;
-    flex-direction: column;
-    justify-content: center; 
-    align-items: center; 
-    text-align: center;
-}
+        .navbar-brand {
+            font-size: 28px;
+            text-transform: uppercase;
+            margin-right: 50px;
+            font-family: 'Playfair Display', serif;
+        }
 
-.welcome-message {
-    font-size: 50px;
-    font-weight: 300;
-    color: #000; 
-    margin: 0;
-    padding: 20px;
-}
+        .navbar .navbar-nav .nav-link {
+            text-transform: uppercase;
+            font-size: 14px;
+        }
+        body {
+            font-family: 'Playfair Display', serif;
+            font-weight: 700;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh; 
+        }
 
-.welcome-image {
-    margin-top: 20px; 
-    margin-right: 50px;
-}
+        body {
+            font-family: 'Playfair Display', serif;
+            font-weight: 700;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh; 
+        }
 
-.welcome-image img {
-    max-width: 100%;
-    height: auto;
-}
+        .content {
+            flex: 1; 
+            display: flex;
+            flex-direction: column;
+            justify-content: center; 
+            align-items: center; 
+            text-align: center;
+        }
+
+        .welcome-message {
+            font-size: 50px;
+            font-weight: 300;
+            color: #000; 
+            margin: 0;
+            padding: 20px;
+        }
+
+        .welcome-image {
+            margin-top: 20px; 
+            margin-right: 50px;
+        }
+
+        .welcome-image img {
+            max-width: 100%;
+            height: auto;
+        }
         </style>
 </head>
 <body>
-<div class="content">
-        <div class="welcome-message">
-            Welcome, {{ $username }}!
-        </div>
-        <div class="welcome-image">
-            <img src="{{ asset('images/hi.png') }}" alt="Welcome Image">
-        </div>
-    </div>
+        <div class="navbar-container">
+                            <!-- Navbar -->
+                            <nav class="navbar navbar-expand-lg">
+                                <div class="container">
+                                    <a class="navbar-brand" href="homepage"></a>
+                                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span class="navbar-toggler-icon"></span>
+                                    </button>
+                                    <div class="collapse navbar-collapse" id="navbarNav">
+                                        <ul class="navbar-nav ms-auto">
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('homepage') }}">Home</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('about1') }}">About Us</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('contact') }}">Contact Us</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
+                        <div class="content">
+                                <div class="welcome-message">
+                                    Welcome, {{ $username }}!
+                                </div>
+                                <div class="welcome-image">
+                                    <img src="{{ asset('images/hi.png') }}" alt="Welcome Image">
+                                </div>
+                            </div>
 </body>
 </html>
