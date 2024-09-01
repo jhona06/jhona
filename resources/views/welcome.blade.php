1dc125bedbef778364d6f49b4115e8ac90ecd5d3
jhona06 <cestinakareljhona6@gmail.com>
2024-09-01 17:44:51 +0800

    resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,45 +4,71 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <style>
+         .navbar-container {
+            position: fixed;
+            top: 0;
+            width: 100%;
+            z-index: 9999;
+        }
 
-body {
-    font-family: 'Playfair Display', serif;
-    font-weight: 700;
-    background-color: #f5f5f5;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh; 
-}
+        .navbar {
+            background-color: #B4D6CD;
+            padding-top: 16px;
+            padding-bottom: 16px;
+        }
 
-.content {
-    flex: 1; 
-    display: flex;
-    flex-direction: column;
-    justify-content: center; 
-    align-items: center; 
-    text-align: center;
-}
+        .navbar-brand {
+            font-size: 28px;
+            text-transform: uppercase;
+            margin-right: 50px;
+            font-family: 'Playfair Display', serif;
+        }
 
-.welcome-message {
-    font-size: 50px;
-    font-weight: 300;
-    color: #000; 
-    margin: 0;
-    padding: 20px;
-}
+        .navbar .navbar-nav .nav-link {
+            text-transform: uppercase;
+            font-size: 14px;
+        }
 
-.welcome-image {
-    margin-top: 20px; 
-    margin-right: 50px;
-}
+        body {
+            font-family: 'Playfair Display', serif;
+            font-weight: 700;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh; 
+        }
 
-.welcome-image img {
-    max-width: 100%;
-    height: auto;
-}
+        .content {
+            flex: 1; 
+            display: flex;
+            flex-direction: column;
+            justify-content: center; 
+            align-items: center; 
+            text-align: center;
+        }
+
+        .welcome-message {
+            font-size: 50px;
+            font-weight: 300;
+            color: #000; 
+            margin: 0;
+            padding: 20px;
+        }
+
+        .welcome-image {
+            margin-top: 20px; 
+            margin-right: 50px;
+        }
+
+        .welcome-image img {
+            max-width: 100%;
+            height: auto;
+        }
         </style>
 </head>
 <body>
@@ -50,7 +76,7 @@ body {
                 <!-- Navbar -->
                 <nav class="navbar navbar-expand-lg">
                     <div class="container">
-                        <a class="navbar-brand" href="macandgab">Mac and Gab</a>
+                        <a class="navbar-brand" href="homepage">Homepage</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
