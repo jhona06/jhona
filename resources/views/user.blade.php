8f8e61104fd70453b47ce47c6c5723c19434fbda
jhona06 <cestinakareljhona6@gmail.com>
2024-09-01 15:14:08 +0800

    resources/views/user.blade.php

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,8 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
+    <style>
+          /* Section Padding */
+          .content-container {
+            padding-top: 100px;
+            padding-bottom: 60px;
+        }
+
+        /* Home Section */
+        #home {
+            background-color: #f8f9fa;
+            padding: 100px 0;
+            text-align: center;
+        }
+
+        #home h1 {
+            font-family: 'Playfair Display', serif;
+            font-weight: 700;
+            margin-bottom: 20px;
+        }
+        </style>
 </head>
 <body>
-    <h1 class="display-3">Welcome, {{ $username }}!</h1>
+    <section id="user" class="py-5 bg-light">
+        <section id="user">
+            <div class="container">
+                <h1 class="display-3"> Welcome, {{ $username }}!</h1>
 </body>
 </html>
