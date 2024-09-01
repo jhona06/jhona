43172d7c7bba06899cc1a813103ed13e6d08da59
jhona06 <cestinakareljhona6@gmail.com>
2024-09-01 15:27:04 +0800

    resources/views/user.blade.php

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
     <style>
+
             body {
                 font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
                 text-align: center;
@@ -18,16 +19,14 @@
                 font-size: 1.5em;
                 color: #333;
                 padding: 30px;
-                border: 1px solid #007bff;
-                border-radius: 12px;
                 display: inline-block;
                 background-color: #ffffff;
+                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
             }
 
             h1 {
                 font-size: 2em;
                 color: #007bff;
-                
             }
 
             .container {
@@ -38,10 +37,10 @@
         </style>
 </head>
 <body>
-        <div class="container">
-                <div class="welcome-message">
-                    <h1>Welcome, {{ $username }}!</h1>
-                </div>
-            </div>
+<div class="container">
+        <div class="welcome-message">
+            <h1>Welcome, {{ $username }}!</h1>
+        </div>
+    </div>
 </body>
 </html>
