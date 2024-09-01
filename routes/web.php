d5f2c85279e9f90b101cf69530d920c8e4f63e51
jhona06 <159889662+jhona06@users.noreply.github.com>
2024-09-01 12:32:26 +0000

    routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -13,34 +13,42 @@ Route::get('/content', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+// it will view the home 
 Route::get('/', function () {
     return view('welcome', ['message' => 'Welcome to the homepage!']);
 })->name('homepage');
 
+// will view the about1 
 Route::get('/about1', function() {
     return view('about1');
 })->name('about1');
 
+// it will redirect the home to "homepage" name
 Route::redirect('/home', "/");
 
+// it will view the contact form page
 Route::get('/contact', function () {
     return view("contact");
 })->name('contact');
 
+// this is the process of getting the http request of the username
 Route::post('/contact/submit', function (Request $request) {
     $username = $request->input('username');
     return redirect()->route('user.optional', ['username' => $username]);
 })->name('user.submit');
 
-
+// this will get the username and constraint the input by letter only
 Route::get('/user/{username}', function ($username) {
     return view('user', ['username' => $username]);
 })->where('username', '[A-Za-z]+')->name('user.show');
 
+// this is will get the username when empty for optional username
 Route::get('/user/{username?}', function ($username = 'Guest') {
     return view('user', ['username' => $username]);
 })->where('username', '[A-Za-z]+')->name('user.optional');
 
+// middleware use for web CSRF protection
 Route::middleware(['web'])->group(function () {
     Route::get('/', function () {
         return view('welcome', ['message' => 'Welcome to our Homepage!']);
