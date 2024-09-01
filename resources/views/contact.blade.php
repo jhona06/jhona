a0add2e07e406d96d1a3af7837dcfe925ff942dc
jhona06 <cestinakareljhona6@gmail.com>
2024-09-01 18:13:19 +0800

    resources/views/contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -86,10 +86,10 @@
                             <div class="collapse navbar-collapse" id="navbarNav">
                                 <ul class="navbar-nav ms-auto">
                                     <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('/') }}">Home</a>
+                                        <a class="nav-link" href="{{ route('homepage') }}">Home</a>
                                     </li>
                                     <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('about') }}">About Us</a>
+                                        <a class="nav-link" href="{{ route('about') }}">About Us</a>
                                     </li>
                                     <li class="nav-item">
                                         <a class="nav-link" href="{{ route('contact') }}">Contact Us</a>
