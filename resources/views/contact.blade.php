1f83aa8193825e5f52c68019d6fe4e711ff909fd
jhona06 <cestinakareljhona6@gmail.com>
2024-09-01 14:30:59 +0800

    resources/views/contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us</title>
+     <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <style>
@@ -47,6 +49,8 @@
             </div>
         </div>
         </section>
+        <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
    
 </body>
 </html>
