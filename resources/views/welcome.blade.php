922dd1da796196b89233037b9843b8bd28186b17
jhona06 <cestinakareljhona6@gmail.com>
2024-09-01 18:37:10 +0800

    resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome Homepage</title>
+    <title>Homepage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
@@ -49,7 +49,7 @@
             justify-content: center; 
             align-items: center; 
             text-align: center;
-            margin-top: 20px;
+            margin-top: 30px;
         }
 
         .welcome-message {
