0e70d0a7c7fcaeb276826c86c5f326f30aaff192
Karel_Jhona <159889662+jhona06@users.noreply.github.com>
2024-09-15 14:56:04 +0800

    routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,86 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use App\Http\Controllers\MenuController;
-// to display the menu
+use Illuminate\Http\Request;\use App\Http\Controllers\MenuController;
+use App\Http\Controllers\PageController;
+
+Route::get('/', [MenuController::class, 'index'])->name('home');
+
 Route::get('/menu', [MenuController::class, 'index'])->name('menu.index');
-// to show the details of a specific menu item
 Route::get('/menu/{id}', [MenuController::class, 'show'])->name('menu.show');
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
 
+Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 
-Route::get('/home', function () {
-    return view('home');
-});
-Route::get('/content', function () {
-    return view('content');
-});
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/', function () {
-    return view('home');
-});
-
-// it will view the home 
-Route::get('/', function () {
-    return view('welcome', ['message' => 'Welcome to the homepage!']);
-})->name('homepage');
-
-Route::get('/about', function() {
-    return view('about');
-})->name('about');
-
-// it will redirect the home to "homepage" name
-Route::redirect('/home', "/");
-
-// it will view the contact form page
-Route::get('/contact', function () {
-    return view("contact");
-})->name('contact');
-
-// Handle form submission and redirect based on username input
-Route::post('/contact/submit', function (Request $request) {
-    $username = $request->input('username');
-    return redirect()->route('user.optional', ['username' => $username]);
-})->name('user.submit');
-
-// this will get the username and constraint the input by letter only
-Route::get('/user/{username}', function ($username) {
-    return view('user', ['username' => $username]);
-})->where('username', '[A-Za-z]+')->name('user.show');
-
-// this is will get the username when empty for optional username
-Route::get('/user/{username?}', function ($username = 'Guest') {
-    return view('user', ['username' => $username]);
-})->where('username', '[A-Za-z]+')->name('user.optional');
-
-// middleware use for web CSRF protection
-Route::middleware(['web'])->group(function () {
-    Route::get('/', function () {
-        return view('welcome', ['message' => 'Welcome to our Homepage!']);
-    })->name('homepage');
-    
-    Route::view('/about1', 'about1')->name('about1');
-    
-    Route::redirect('/home', '/');
-
-    Route::get('/contact', function () {
-        return view('contact');
-    })->name('contact');
-    
-    Route::prefix('user')->group(function () {
-        Route::get('/{username?}', function ($username = 'Guest') {
-            return view('user', ['username' => $username]);
-        })->where('username', '[A-Za-z]+')->name('user.optional');
-    });
-});
-
-
+Route::post('/contact', [PageController::class, 'send'])->name('contact.send');
 
