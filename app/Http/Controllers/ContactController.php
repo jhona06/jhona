238e1099360b465b627de5f01eb3fa825f6c1c36
jhona06 <cestinakareljhona6@gmail.com>
2024-09-15 16:57:26 +0800

    app/Http/Controllers/ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,17 +5,7 @@ use App\Models\Contact;
 
 use Illuminate\Http\Request;
 
-class ContactController extends Controller
-{
-    public function submit(Request $request)
-    {
-        // Handle the form submission
-        // Validate and process the request
 
-        // For now, just redirect back with a success message
-        return redirect()->route('contact')->with('success', 'Your message has been sent!');
-    }
-}
 class ContactController extends Controller
 {
     public function submit(Request $request)
