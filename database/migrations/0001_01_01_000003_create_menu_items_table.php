3cc9b519c24943f5ef72377ed13c491ba54f60e7
Karel_Jhona <159889662+jhona06@users.noreply.github.com>
2024-09-15 14:42:55 +0800

    database/migrations/0001_01_01_000003_create_menu_items_table.php

--- database/migrations/0001_01_01_000003_create_menu_items_table.php
+++ database/migrations/0001_01_01_000003_create_menu_items_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+return new class extends Migration
+{
 public function up()
 {
     Schema::create('menu_items', function (Blueprint $table) {
@@ -17,4 +19,5 @@ public function up()
 public function down()
 {
     Schema::dropIfExists('menu_items');
+}
 }
