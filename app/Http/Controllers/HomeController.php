1f6cb4bda3b8edd6caecdd41bffbfb78808a8b2f
Karel_Jhona <159889662+jhona06@users.noreply.github.com>
2024-09-15 18:00:34 +0800

    app/Http/Controllers/HomeController.php

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MenuItem;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        // Fetch all menu items from the database
+        $menuItems = MenuItem::all();
+
+        // Pass the menu items to the view
+        return view('home', ['menuItems' => $menuItems]);
+    }
+}
