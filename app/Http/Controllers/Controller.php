8cb0e6b457db1c5f83ef497c967b5898a589f5a9
Karel_Jhona <159889662+jhona06@users.noreply.github.com>
2024-09-15 17:33:06 +0800

    app/Http/Controllers/Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,19 +6,13 @@ use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
+   
     public function index()
     {
         // Fetch all menu items from the database
         $menuItems = MenuItem::all();
-        // Return the view with menu items
-        return view('home', compact('menuItems'));
-    }
 
-    public function show($id)
-    {
-        // Find a specific menu item by ID
-        $menuItem = MenuItem::findOrFail($id);
-        // Return the view with the specific menu item
-        return view('menu.show', compact('menuItem'));
+        // Pass the menu items to the view
+        return view('home', ['menuItems' => $menuItems]);
     }
 }
