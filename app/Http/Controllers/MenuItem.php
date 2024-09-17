5feb464b7db6169da7ef46cfb8d1332da16c309b
Karel_Jhona <159889662+jhona06@users.noreply.github.com>
2024-09-17 22:29:47 +0800

    app/Http/Controllers/MenuItem.php

--- app/Http/Controllers/MenuItem.php
+++ app/Http/Controllers/MenuItem.php
@@ -1,29 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Models;
 
-use App\Models\Category;
-use App\Models\MenuItem;
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
 
-class HomeController extends Controller
+class MenuItem extends Model
 {
-    public function index(Request $request)
-    {
-        $categories = Category::all();
-        $query = MenuItem::query();
-
-        if ($request->has('category_id')) {
-            $query->where('category_id', $request->input('category_id'));
-        }
-
-        if ($request->has('search')) {
-            $query->where('name', 'like', '%' . $request->input('search') . '%');
-        }
-
-        $menuItems = $query->get();
-
-        return view('home', compact('categories', 'menuItems'));
-    }
+    protected $fillable = ['name', 'price', 'category_id', 'image'];
 }
-
